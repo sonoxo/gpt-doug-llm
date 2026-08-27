231e4d0f035b462cdf7705bc46daff2dd234940b
Doug Brown <60854716+sonoxo@users.noreply.github.com>
2026-08-27 05:46:22 -0400

    Add ZYRA Intelligence Cloud package

---
+++ agency_cloud/__init__.py
@@ -0,0 +1,7 @@
+"""ZYRA Intelligence Cloud.
+
+Private business-intelligence and cyber-defense platform.
+This package does not confer or represent governmental authority.
+"""
+
+__version__ = "0.1.0"
