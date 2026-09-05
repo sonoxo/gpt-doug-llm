01bc1d10647c6500d6a4595a288867f16ee5d454
Douglas Brown .Jr <60854716+sonoxo@users.noreply.github.com>
2026-09-05 01:52:22 -0400

    Add GrimTheBuilder Pages interaction

---
+++ docs/grimthebuilder/script.js
@@ -0,0 +1,8 @@
+const button = document.querySelector('#launch');
+
+button.addEventListener('click', () => {
+  button.textContent = 'XUNIA LINK ONLINE ✓';
+  console.log('GrimTheBuilder is connected to XUNIA HQ.');
+});
+
+console.log('GrimTheBuilder // XUNIA HQ connection loaded.');
