4ad3dbcf2bac1f500c1ed7105ea3eb8f86451502
Douglas M Brown <zyra.localhost@gmail.com>
2026-09-13 17:18:54 -0400

    feat(cte): add durable execution journal

--- research_lab/cte/__init__.py
+++ research_lab/cte/__init__.py
@@ -1,9 +1,13 @@
 from .authorization import ReceiptAuthorizer, build_authorization_snapshot
 from .engine import CounterfactualTransactionEngine
+from .journal import ExecutionJournal, JournalAttempt, JournalEvent
 from .models import ProposedTransition, StateSnapshot
 
 __all__ = [
     "CounterfactualTransactionEngine",
+    "ExecutionJournal",
+    "JournalAttempt",
+    "JournalEvent",
     "ProposedTransition",
     "ReceiptAuthorizer",
     "StateSnapshot",
