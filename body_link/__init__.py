8ad3877634998787d09019ac46188c004dc4b342
Douglas Brown .Jr <60854716+sonoxo@users.noreply.github.com>
2026-09-20 20:33:52 -0400

    Export Body-Link V2 state primitives

--- body_link/__init__.py
+++ body_link/__init__.py
@@ -2,5 +2,15 @@
 
 from .client import BodyLinkClient, BodyLinkError
 from .protocol import normalize_runtime_url
+from .relay import BodyStateRelay
+from .state import ALLOWED_BODY_STATES, runtime_body_state, sanitize_body_state
 
-__all__ = ["BodyLinkClient", "BodyLinkError", "normalize_runtime_url"]
+__all__ = [
+    "BodyLinkClient",
+    "BodyLinkError",
+    "BodyStateRelay",
+    "ALLOWED_BODY_STATES",
+    "runtime_body_state",
+    "sanitize_body_state",
+    "normalize_runtime_url",
+]
